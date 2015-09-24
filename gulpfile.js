8750a8f6b3269b72a805a182f1349feca9ff8e21
Jill Ducey <Jill.Ducey@perceptivesoftware.com>
2015-09-24 13:07:29 -0500

    Added sass to watch and watch to default

--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,7 @@ gulp.task('compass', function() {
 gulp.task('watch', function(){
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss', ['compass']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
